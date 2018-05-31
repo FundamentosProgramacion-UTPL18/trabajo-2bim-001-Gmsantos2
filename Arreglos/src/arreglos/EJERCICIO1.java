6bea8412690f49dc9a3ae7f6062a7d11af2a4e97
Gerson Santos <gersantos1234@gmail.com>
2018-05-31 10:12:22 -0500

    Agregando ejercicio arreglo

---
+++ Arreglos/src/arreglos/EJERCICIO1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EJERCICIO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int lista[];
+      lista= new int [4];
+      lista[0]=10;
+      for (int cont=0 ; cont<=(lista.length);cont ++){
+        System.out.println(lista[cont]);
+      }
+   }
+}
+    
+
